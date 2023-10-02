fc8b203ab1cba890f6337df61cf44cbc415aa390
Farshid-997 <arnob.farshid@gmail.com>
2023-10-02 17:57:48 +0600

    first commit3

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send(" server is working...");
+  res.send("server is working...");
 });
 
 module.exports = { app };
